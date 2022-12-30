1478b325568a9b56a416186dd6508ca527408056
tanskymanagement@gmail.com <tanskymanagement@gmail.com>
2022-12-30 11:07:33 -0500

    Lecture de la carte

--- pythonAPP/Services/RequestSendAlert.py
+++ pythonAPP/Services/RequestSendAlert.py
@@ -4,7 +4,7 @@ from Domain.GeoMap import *
 
 
 def EnvoyerSignalement(nom_complet, nom_camera, ip, geomap):
-    url = 'http://localhost:8080/myprojects/iisprotect-AI/backendPHP/Services/sendName.php'
+    url = 'http://localhost:8080/myprojects/iisprotect-AI/projet-AI-Face-Recognition-To-Server/backendPHP/Services/sendName.php'
     res = requests.post(
         url, params={"nom_du_suspect": nom_complet, "ip": ip, "longitude": geomap._longitude, "latitude": geomap._latitude, "nom_camera": nom_camera})
     print(res.text)
